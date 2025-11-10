6b33876c4ed6fbcf2afbb916dce5e0e21add7f3c
Hasnat <hasnatmillion@gmail.com>
2025-11-10 10:24:40 +0500

    Updated the build and dot env

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
